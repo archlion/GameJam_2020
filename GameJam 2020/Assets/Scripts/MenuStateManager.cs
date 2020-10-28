67ca121eb59a4fcbc08691ae9b28dd59bb95ab02
Renato Francói Amprino <renato.francoi.amprino@gmail.com>
2020-10-27 21:40:08 -0300

    HUD and UI first stuff

---
+++ GameJam 2020/Assets/Scripts/MenuStateManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuStateManager : MonoBehaviour
+{
+    public bool stateIdle = true;
+    Animator menuAnim;
+
+    void Start()
+    {
+        menuAnim = GetComponent<Animator>();
+    }
+
+    public void MenuChange(int state)                           //função chamada pelos botões.
+    {
+        if (stateIdle)                                          //só permite uma mudança de estado caso outra não esteja acontecendo
+        {
+            menuAnim.SetInteger("stateParameter", state);       //sinaliza a mudança de estado do menu
+            stateIdle = false;                                  //sinaliza o início da animação de mudança de estado
+            Debug.Log("Changing to state " + state);
+        }
+        else Debug.LogWarning("Menu not idle! Can't change state until it's finished changing states.");
+    }
+
+    public void MenuChangeComplete()                            //sinaliza o fim da mudança de estado
+    {
+        if (!stateIdle)
+        { 
+            stateIdle = true;
+            Debug.Log("Finished! Menu is now idle.");
+        }
+    }
+
+}
---
+++ GameJam 2020/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    Animator sceneAnim;
+    int otherScene;
+    int currentScene;
+
+    void Start()
+    {
+        sceneAnim = GetComponent<Animator>();
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        switch (currentScene)
+        {
+            case 0:                          //em caso de entrada em cena do menu
+                otherScene = 1;
+                break;
+
+            case 1:                          //em caso de entrada em cena do jogo
+                otherScene = 0;
+                sceneAnim.SetBool("enteringScene", true);
+                break;
+        }
+    }
+
+    public void SwitchScene()                           //é chamada por algum script ligado a um botão para iniciar a transição de cena
+    {
+        sceneAnim.SetBool("exitingScene", true);        //ativa a animação de saída
+    }
+
+    public void LoadNewScene()                          //é chamada pela animação de saída
+    {
+        Debug.Log("Loading scene " + otherScene + ".");
+        //sceneAnim.SetBool("exitingScene", false);
+        SceneManager.LoadSceneAsync(otherScene);         // carrega a outra cena
+    }
+
+    public void SceneChangeComplete()
+    {
+        Debug.Log("Finished changing to scene " + currentScene + ".");
+        sceneAnim.SetBool("enteringScene", false);
+    }
+
+    public void QuitGame()                              //fecha o jogo
+    {
+        Application.Quit();
+        Debug.Log("Game would have quit.");
+    }
+}
