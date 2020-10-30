46a71cb8df3621d9ccc1af5dbaffa2513b24fa95
arch <jamesgardenhall@gmail.com>
2020-10-29 23:43:33 -0300

    Auto stash before merge of "main" and "origin/main"
    Combat

--- GameJam 2020/Assets/PlayerMovement.cs
+++ GameJam 2020/Assets/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -17,13 +18,23 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
     }
 
-    void FixedUpdate()
+    public void OnLanding()
+    {
+
+        animator.SetBool("IsJumping", false);
+
+    }
+     void FixedUpdate()
     {
 
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
